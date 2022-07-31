4d7f1b019ce03288a85a6de937a395bf758c2026
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-07-31 17:20:10 +0300

    Install DI Framework

---
+++ Assets/Scripts/Di/GameCompositionRoot.cs
@@ -0,0 +1,10 @@
+﻿using DELTation.DIFramework;
+using DELTation.DIFramework.Containers;
+
+namespace Di
+{
+    public class GameCompositionRoot : DependencyContainerBase
+    {
+        protected override void ComposeDependencies(ICanRegisterContainerBuilder builder) { }
+    }
+}
